e1830be44540345e2f738b5b7a8f41d0e4a3be45
skipancho <ctg1770a@gmail.com>
2022-01-28 15:07:44 +0900

    불량 사용자 (dfs, 성공)

--- src/main/kotlin/lv3/Bad_user.kt
+++ src/main/kotlin/lv3/Bad_user.kt
@@ -1,48 +1,72 @@
 package lv3
+/*
+개발팀 내에서 이벤트 개발을 담당하고 있는 "무지"는 최근 진행된 카카오이모티콘 이벤트에 비정상적인 방법으로 당첨을 시도한 응모자들을 발견하였습니다. 이런 응모자들을 따로 모아 불량 사용자라는 이름으로 목록을 만들어서 당첨 처리 시 제외하도록 이벤트 당첨자 담당자인 "프로도" 에게 전달하려고 합니다. 이 때 개인정보 보호을 위해 사용자 아이디 중 일부 문자를 '*' 문자로 가려서 전달했습니다. 가리고자 하는 문자 하나에 '*' 문자 하나를 사용하였고 아이디 당 최소 하나 이상의 '*' 문자를 사용하였습니다.
+"무지"와 "프로도"는 불량 사용자 목록에 매핑된 응모자 아이디를 제재 아이디 라고 부르기로 하였습니다.
 
+예를 들어, 이벤트에 응모한 전체 사용자 아이디 목록이 다음과 같다면
+
+응모자 아이디
+frodo
+fradi
+crodo
+abc123
+frodoc
+다음과 같이 불량 사용자 아이디 목록이 전달된 경우,
+
+불량 사용자
+fr*d*
+abc1**
+불량 사용자에 매핑되어 당첨에서 제외되어야 야 할 제재 아이디 목록은 다음과 같이 두 가지 경우가 있을 수 있습니다.
+
+제재 아이디
+frodo
+abc123
+제재 아이디
+fradi
+abc123
+이벤트 응모자 아이디 목록이 담긴 배열 user_id와 불량 사용자 아이디 목록이 담긴 배열 banned_id가 매개변수로 주어질 때, 당첨에서 제외되어야 할 제재 아이디 목록은 몇가지 경우의 수가 가능한 지 return 하도록 solution 함수를 완성해주세요.
+
+[제한사항]
+user_id 배열의 크기는 1 이상 8 이하입니다.
+user_id 배열 각 원소들의 값은 길이가 1 이상 8 이하인 문자열입니다.
+응모한 사용자 아이디들은 서로 중복되지 않습니다.
+응모한 사용자 아이디는 알파벳 소문자와 숫자로만으로 구성되어 있습니다.
+banned_id 배열의 크기는 1 이상 user_id 배열의 크기 이하입니다.
+banned_id 배열 각 원소들의 값은 길이가 1 이상 8 이하인 문자열입니다.
+불량 사용자 아이디는 알파벳 소문자와 숫자, 가리기 위한 문자 '*' 로만 이루어져 있습니다.
+불량 사용자 아이디는 '*' 문자를 하나 이상 포함하고 있습니다.
+불량 사용자 아이디 하나는 응모자 아이디 중 하나에 해당하고 같은 응모자 아이디가 중복해서 제재 아이디 목록에 들어가는 경우는 없습니다.
+제재 아이디 목록들을 구했을 때 아이디들이 나열된 순서와 관계없이 아이디 목록의 내용이 동일하다면 같은 것으로 처리하여 하나로 세면 됩니다.
+ */
 class Bad_user {
+    val hashSet = HashSet<HashSet<String>>()
+    lateinit var ids : Array<String>
+    lateinit var bans : Array<String>
     fun solution(user_id: Array<String>, banned_id: Array<String>): Int {
-        var answer = 1
-        val cntMap = HashMap<String,Int>()
-        val caseMap = HashMap<String,Int>()
-        val userMap = HashMap<String,Int>()
+        ids = user_id
+        bans = banned_id
 
-        for(ban in banned_id){
-            var case = 0
-            if (caseMap[ban] != null){
-                val cnt = cntMap.getOrDefault(ban,0)
-                cntMap[ban] = cnt + 1
-                continue
-            }
-            for(user in user_id){
-                if (idCheck(user,ban)){
-                    val cnt = userMap.getOrDefault(user,0)
-                    userMap[user] = cnt + 1
-                    case++
-                }
-            }
-            cntMap[ban] = 1
-            caseMap[ban] = case
-        }
+        dfs(null,0,HashSet<String>(),bans.size)
 
-        cntMap.forEach { (t, u) ->
-            var case = caseMap.getOrDefault(t,1)
-            if (u > 1){
-                var tmp = factorial(case,u,1)
-                tmp /= factorial(u,u,1)
-                case = tmp
-            }
-            answer *= case
-        }
-        userMap.forEach { (t, u) ->
-            if (u > 1){
-                val tmp = dupleCheck(u)
-                answer -= tmp
-            }
-        }
-
-        return answer
+        return hashSet.size
     }
+
+    fun dfs(pos : String?, d : Int, set : HashSet<String>,n : Int){
+        val newSet = set.map { it }.toHashSet()
+        if (pos != null ) newSet.add(pos)
+
+        if (d == n){
+            if (newSet.size == n)
+                hashSet.add(newSet)
+            return
+        }
+
+        for (id in ids){
+            if (idCheck(id , bans[d]))
+                dfs(id,d+1, newSet,n)
+        }
+    }
+
     fun idCheck(id : String, ban : String ) : Boolean{
         if (id.length != ban.length){
             return false
@@ -53,22 +77,5 @@ class Bad_user {
             }
         }
         return true
-    }
-
-    fun dupleCheck(n : Int) : Int{
-        var temp = 0
-        for (i in 2..n){
-            var temp2 = factorial(n,i,1) / factorial(i,i,1)
-            temp += temp2
-        }
-        return temp
-    }
-
-    fun factorial(n:Int, cnt:Int,result : Int) : Int{
-        return if (cnt == 0){
-            result
-        }else{
-            factorial(n-1,cnt-1,result*n)
-        }
     }
 }
