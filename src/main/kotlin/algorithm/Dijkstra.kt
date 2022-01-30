fe15c73e5b5ef7f70796ea499ad8b50a088569bb
skipancho <ctg1770a@gmail.com>
2022-01-30 14:01:07 +0900

    알고리즘 - Dijkstra

---
+++ src/main/kotlin/algorithm/Dijkstra.kt
@@ -0,0 +1,41 @@
+package algorithm
+
+import java.util.*
+
+class Dijkstra {
+
+    fun dijkstra(n : Int, start : Int, end : Int, edges : Array<IntArray>) : Int{
+        val dist = IntArray(n+1){2000000}
+        val route = Array(n+1){ArrayList<Node>()}
+        val queue = PriorityQueue<Node>()
+
+        for (edge in edges){
+            route[edge[0]].add(Node(edge[1],edge[2]))
+            // 양방향일 경우
+            //route[edge[1]].add(Node(edge[0],edge[2]))
+        }
+        dist[start] = 0
+        queue.add(Node(start, 0))
+        while (queue.isNotEmpty()){
+            val node = queue.poll()
+            val cur_idx = node.idx
+            val cur_dist = node.dist
+            if (dist[cur_idx] < cur_dist) continue
+
+            for (i in route[cur_idx]){
+                val next_dist = cur_dist + i.dist
+                if (next_dist < dist[i.idx]){
+                    dist[i.idx] = next_dist
+                    queue.add(Node(i.idx, next_dist))
+                }
+            }
+        }
+        return dist[end]
+    }
+    data class Node(
+        val idx : Int,
+        val dist : Int
+    ) : Comparable<Node>{
+        override fun compareTo(other: Node) = dist - other.dist
+    }
+}
