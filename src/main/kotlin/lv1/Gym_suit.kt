760ab89f642b08bae4a15916f67e6aef7525bc24
skipancho <ctg1770a@gmail.com>
2022-02-21 20:07:50 +0900

    lv1 체육복 다른 풀이 추가

--- src/main/kotlin/lv1/Gym_suit.kt
+++ src/main/kotlin/lv1/Gym_suit.kt
@@ -45,4 +45,19 @@ class Gym_suit {
         }
         return answer
     }
+
+    fun solution2(n: Int, lost: IntArray, reserve: IntArray): Int {
+        var answer = n
+        val lostSet = lost.toSet() - reserve.toSet()
+        val reserveSet = (reserve.toSet() - lost.toSet()) as MutableSet
+
+        for (i in lostSet) {
+            when {
+                i + 1 in reserveSet -> reserveSet.remove(i + 1)
+                i - 1 in reserveSet -> reserveSet.remove(i - 1)
+                else -> answer--
+            }
+        }
+        return answer
+    }
 }
