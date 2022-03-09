bfa8e4340ffd90282bd8b5f0321cc143e7e2c836
skipancho <ctg1770a@gmail.com>
2022-03-09 20:33:55 +0900

    lv1 문자열 다루기 기본 풀이

---
+++ src/main/kotlin/lv1/StringControl.kt
@@ -0,0 +1,16 @@
+package lv1
+/*
+문제 설명
+문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
+제한 사항
+s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+입출력 예
+s	return
+"a234"	false
+"1234"	true
+ */
+class StringControl {
+    fun solution(s: String): Boolean =
+        s.matches("[0-9]{4}|[0-9]{6}".toRegex())
+}
